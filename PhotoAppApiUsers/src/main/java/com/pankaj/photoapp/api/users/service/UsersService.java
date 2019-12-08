bbb8ddaad4e53ce7cb3b33a62a0d7f3109134f2a
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-08 21:38:10 +0530

    Implemented a serilaized shared dto class to be used in UsersService and UsersServiceImpl class

---
+++ PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.pankaj.photoapp.api.users.service;
+
+import com.pankaj.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+	
+	UserDto createUser(UserDto userDetails);
+
+}
---
+++ PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pankaj.photoapp.api.users.service;
+
+import com.pankaj.photoapp.api.users.shared.UserDto;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
