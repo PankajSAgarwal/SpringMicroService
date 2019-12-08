90c89e27044c84ce84e1cc6b18c147ad122db9b1
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-08 21:07:59 +0530

    Adding RequestBody annotation to UserController

--- PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser(CreateUserRequestModel userDetails) {
+	public String createUser(@RequestBody CreateUserRequestModel userDetails) {
 		
 		return "Create user method is called";
 	}
