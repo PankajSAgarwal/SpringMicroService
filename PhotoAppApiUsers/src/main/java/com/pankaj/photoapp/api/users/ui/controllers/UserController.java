c4c1753ecdd818f874a26df848b56f1a4cd99691
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-08 13:49:35 +0530

    1.Added method createUser in PhotoApiUserController  to handle HTTP POST request 2.Create User Request model

--- PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.pankaj.photoapp.api.users.ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pankaj.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +19,12 @@ public class UserController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port "+env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser(CreateUserRequestModel userDetails) {
+		
+		return "Create user method is called";
 	}
 
 }
