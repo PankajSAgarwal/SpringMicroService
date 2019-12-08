3c49c487c59aeeea521a35f49c7fda8cf34f9b66
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-08 22:56:50 +0530

    Return HTTP Status code 201 on creation of user in UserController class

--- PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/pankaj/photoapp/api/users/ui/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +33,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+	public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
@@ -39,7 +41,7 @@ public class UserController {
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 		usersService.createUser(userDto);
 		
-		return "Create user method is called";
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
 
 }
